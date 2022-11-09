a9a64edd942556e15f4c2c318354b34598077e3f
NikjokesApart <108344750+NikjokesApart@users.noreply.github.com>
2022-11-10 01:09:02 +0300

    10-11

--- java-sprint5-hw/src/InMemoryHistoryManager.java
+++ java-sprint5-hw/src/InMemoryHistoryManager.java
@@ -1,8 +1,15 @@
 import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
+    private static final int SIZE_OF_HISTORY = 10;
+    final private Map<Integer, Node<Task>> tasksIdAndNodes = new HashMap<>();
+    private Node<Task> head;
+    private Node<Task> tail;
+    private int sizeOfCustomLinkedList = 0;
+
     @Override
     public void add(Task task) {
+
     }
 
     @Override
@@ -10,84 +17,63 @@ public class InMemoryHistoryManager implements HistoryManager {
     }
 
     private static class Node<Task>{
-        private Task task;
+        private Task data;
         private Node<Task> prev;
         private Node<Task> next;
-        private Node<Task> head;
+
+
+        private Node(Node<Task> prev,Task data, Node<Task> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
 
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
-            Node<Task> node = (Node<Task>) o;
-            return sizeOfCustomLinkedList == node.sizeOfCustomLinkedList && Objects.equals(task, node.task) && Objects.equals(prev, node.prev) && Objects.equals(next, node.next) && Objects.equals(tasksIdAndNodes, node.tasksIdAndNodes) && Objects.equals(head, node.head) && Objects.equals(next, node.next);
+            Node<?> node = (Node<?>) o;
+            return Objects.equals(data, node.data) && Objects.equals(prev, node.prev) && Objects.equals(next, node.next);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(task, prev, next, tasksIdAndNodes, head, next, sizeOfCustomLinkedList);
-        }
-
-        private Node(Task task, Node<Task> prev, Node<Task> next) {
-            this.task = task;
-            this.prev = prev;
-            this.next = next;
-        }
-
-        public String toString() {
-            String result = "Post{task='" + task + "',tasksIdAndNodes.size()='" + tasksIdAndNodes.size() + "',prev=" + prev + ",next=" + next + '}';
-            if (tasksIdAndNodes != null) {
-            } else {
-                result = result + ", tasksIdAndNodes=null";
-            }
-            return result;
-        }
-
-        private static final int SIZE_OF_HISTORY = 10;
-        final private Map<Integer, Node<Task>> tasksIdAndNodes = new HashMap<>();
-        private int sizeOfCustomLinkedList = 0;
-
-        private ArrayList<Task> getTasks() {
-            ArrayList<Task> tasks = new ArrayList<>();
-            Node<Task> node = head;
-            while (node.next != null) {
-                tasks.add((Task) node);
-            }
-            return tasks;
-        }
-
-        private void linkLast(Task task) {
-            final Node<Task> node = new Node<Task>(task, prev, null);
-            prev = node;
-        }
-
-              public void add(Task task) {
-            removeNode(task);
-            getTasks();
-        }
-
-        private void removeNode(Task id) {
-            final Node<Task> node = tasksIdAndNodes.remove(id);
-            if (node.prev == null) {
-                node.next.prev = null;
-            } else if (node.prev != null && node != null) {
-                node.next.prev = null;
-            }
-        }
-
-        public void addLast(Task task) {
-            final Node<Task> oldTail = prev;
-            final Node<Task> newNode = new Node(task, prev, null);
-            prev = newNode;
-            if (oldTail == null) {
-                head = newNode;
-                sizeOfCustomLinkedList++;
-            } else {
-                oldTail.next = newNode;
-                sizeOfCustomLinkedList++;
-            }
+            return Objects.hash(data, prev, next);
         }
     }
+
+    /*private ArrayList<Task> getTasks() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        Node<Task> node = head;
+        while (node.next != null) {
+            tasks.add((Task) node);
+        }
+        return tasks;
+    }*/
+
+    private void removeNode(Task id) {
+        final Node<Task> node = tasksIdAndNodes.remove(id);
+        if (node.prev == null) {
+            node.next.prev = null;
+        } else if (node.prev != null && node != null) {
+            node.next.prev = null;
+        }
+    }
+
+
+    private void linkLast (Task task) {
+        final Node<Task> oldTail = tail;
+        final Node<Task> newNode = new Node<>(oldTail, task, null);
+        tail = newNode;
+        if(oldTail == null) {
+            head = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+        ++sizeOfCustomLinkedList;
+        tasksIdAndNodes.put(task.getTaskById(),tail);
+    }
+
 }
 
 
